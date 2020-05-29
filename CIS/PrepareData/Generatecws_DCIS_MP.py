94da48d1c7f7db75753c4659f7cae6b16b27b308
pathdata <56313819+pathdata@users.noreply.github.com>
2020-05-29 10:43:03 +0100

    updated comments
    updated comments

--- CIS/PrepareData/Generatecws_DCIS_MP.py
+++ CIS/PrepareData/Generatecws_DCIS_MP.py
@@ -4,6 +4,9 @@ Created on Mon Nov 20 18:16:05 2017
 
 @author: pnarayanan
 """
+# Dependencies on openslide library 'https://openslide.org/'
+# PIL and python
+# Prepare tiles for processing using multiprocessing support on a regular cpu # 
 
 import openslide
 import os
