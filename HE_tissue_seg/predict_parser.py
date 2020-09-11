8991ee133b598b92f3dfd426d6c41fd218f923f9
YemanBrhane <yemanbrhane1989@gmail.com>
2020-09-11 18:06:17 +0100

    Update predict_parser.py
    added default value for model

--- HE_tissue_seg/predict_parser.py
+++ HE_tissue_seg/predict_parser.py
@@ -2,7 +2,7 @@ import argparse
 
 def get_parsed_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-model', '--model', dest='model', help='path to the model')
+    parser.add_argument('-model', '--model', dest='model', help='path to the model', default='specify the path')
     parser.add_argument('-test', '--test', dest='test', help='path to test images')
     parser.add_argument('-result', '--result', dest='result', help='path to predicted result images')
     args = parser.parse_args()
