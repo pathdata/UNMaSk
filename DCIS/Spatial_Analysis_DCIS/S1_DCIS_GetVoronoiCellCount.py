a0b72ae90acd81072b6b99f35ed4a16254aec2e0
Priya Narayanan <56313819+pathdata@users.noreply.github.com>
2020-12-17 13:26:08 +0000

    Need to add about spatial analysis to readme

---
+++ DCIS/Spatial_Analysis_DCIS/S1_DCIS_GetVoronoiCellCount.py
@@ -0,0 +1,72 @@
+
+
+import io
+import json
+from skimage import io
+from shapely.geometry import Polygon, Point
+import matplotlib.pyplot as plt
+import seaborn as sns
+import  numpy as np
+import pandas as pd
+import os
+
+
+
+im_dir = r'D:/DCIS_2019/PureDCIS_Tissuemask'
+voronoi_dir = r'D:/DCIS_2019/PureDCIS_Voronoi'
+cells_dir = r'D:/DCIS_2019/PureDCIS_cellPos_csv'
+
+
+def computevoronoicellcount(cell_names):
+
+    for ii, file_name in enumerate(os.listdir(im_dir)):
+        print(file_name)
+
+        slide_cell_count_df  = pd.DataFrame(columns=[['voronoi_n']+ cell_names])
+
+        print('processing, file:{},..... {}/{}'.format(os.path.splitext(file_name)[0],ii + 1,len(os.listdir(im_dir))))
+
+        cell_pos_df = pd.read_csv(os.path.join(cells_dir, os.path.splitext(file_name)[0]+'.csv'))
+        cell_pos_df = cell_pos_df[cell_pos_df['class']!='notcell']
+        #print(os.path.join(voronoi_dir, os.path.splitext(file_name)[0]+'.jpg'))
+
+        voronoi_file = os.path.join(voronoi_dir, file_name,'voronoi_data.json')
+        print(voronoi_file)
+        if os.path.isfile(voronoi_file) is True:
+            with open(voronoi_file, 'r') as file:
+                voronoi_data = json.load(file)
+        else:
+            print('no voronoi data provided')
+            continue
+        for i in range(len(voronoi_data)):
+            poly_vertices = voronoi_data['voronoi_{}'.format(i)]
+
+            polygon_cell_count_df = getCellCountInsidePolygon(df=cell_pos_df,
+                                                               poly_vert=poly_vertices)
+            counts = ['voronoi_{}'.format(i)]
+            for cell_name in cell_names:
+                if cell_name in list(polygon_cell_count_df.columns):
+                    counts.append(polygon_cell_count_df.loc['N',cell_name])
+                else:
+                    counts.append(0)
+            n = len(slide_cell_count_df)
+            slide_cell_count_df.loc[n]=counts
+
+        slide_cell_count_df.to_csv(os.path.join(voronoi_dir, os.path.splitext(file_name)[0]+'.jpg', 'voronoi_cell_count.csv'), index=False)
+
+
+def getCellCountInsidePolygon(df, poly_vert):
+    polygon = Polygon(poly_vert)
+
+    is_inside_polygon = df.apply(lambda row: polygon.contains(Point(row['x'], row['y'])), axis=1)
+
+    df_inside_poly = df[is_inside_polygon]
+
+    grouped_df = df_inside_poly.groupby(by='class').count().reset_index().set_index('class')[['x']].rename(
+        columns={'x': 'N'}).transpose()
+    return grouped_df
+
+
+if __name__=='__main__':
+    cell_names  = [ 'f', 'l', 't', 'o']
+    computevoronoicellcount(cell_names=cell_names)
---
+++ DCIS/Spatial_Analysis_DCIS/get_duct_segm_local.m
@@ -0,0 +1,86 @@
+function[ ] = get_duct_segm_local(  )
+%Create whole-slide duct segmentation mask and ecological ROI
+
+imagesPath='DCIS_Duke/Data/cws/';                %cws
+cellClassPath = 'DL_Class_Results/20200131/csv/';%cellclassification_csv
+ductSegmPath = 'DL_DuctSegm/20200104/mat/';      %mat
+outputPath = 'Duct_Eco/Duct_Segm/';              %output
+
+folders = dir(fullfile(imagesPath, 'DCIS*'));
+isub = [folders(:).isdir];      
+folders = {folders(isub).name}';
+folders(ismember(folders,{'.','..'})) = [];
+
+duct_nbh_size = 10;
+scale = 16;
+tile_scaled_size = 2000/scale;
+se = strel('disk',5);
+
+for i=1:length(folders)
+    fprintf('Processing %s \n', folders{i})
+    load(fullfile(imagesPath, folders{i}, 'param.mat'));
+    tiles_ids = reshape(1:ceil(slide_dimension(1)/2000)* ceil(slide_dimension(2)/2000), ...
+        [ceil(slide_dimension(1)/2000), ceil(slide_dimension(2)/2000)]);
+    tiles_ids = tiles_ids.'-1;
+    slide_mask = zeros((size(tiles_ids,1)-1)*tile_scaled_size, (size(tiles_ids,2)-1)*tile_scaled_size);
+    for c = 1:size(tiles_ids,2)
+        for r = 1:size(tiles_ids,1)
+            fprintf('Processing %d tile \n', tiles_ids(r,c))
+            mat=load(fullfile(ductSegmPath, folders{i}, strcat('Da', num2str(tiles_ids(r,c)), '.mat')));
+            if isfield(mat, 'mat')
+                da_mask=mat.mat.BinLabel;
+            else
+                da_mask=mat.output(:,:,1)<0.5;
+            end
+            da_mask = imopen(da_mask,se);
+            da_mask = imclose(da_mask,se);
+            
+            %Remove ducts with too few cancer cell detections
+            filename = fullfile(cellClassPath,folders{i}, strcat('Da', num2str(tiles_ids(r,c)), '.csv'));
+            delimiter = ',';
+            startRow = 2;
+            formatSpec = '%q%f%f%[^\n\r]';
+            fileID = fopen(fullfile(filename),'r');
+            if fileID~=-1
+                dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, ...
+                    'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+           
+            fclose(fileID);
+            dataArray([2, 3]) = cellfun(@(x) num2cell(x), dataArray([2, 3]), 'UniformOutput', false);
+            DaCellPos = [dataArray{1:end-1}];
+            Da_t = DaCellPos(strcmp('t',DaCellPos(:,1)),:);
+            cc_da = bwconncomp(da_mask);
+            for cc=1:cc_da.NumObjects 
+                count=0;
+                for tc = 1:size(Da_t,1)
+                    cellInd = sub2ind(size(da_mask), Da_t{tc,3}, Da_t{tc,2});
+                    if ~isempty(find(cc_da.PixelIdxList{1,cc}==cellInd, 1))
+                        count = count+1;
+                    end
+                end
+                if count < 5
+                    da_mask(cc_da.PixelIdxList{1,cc}) = 0;
+                end
+            end
+            
+            else 
+                da_mask = zeros(size(da_mask));
+            end
+            
+            tile_m = imresize(da_mask, 1/scale);
+            slide_mask(((r-1)*tile_scaled_size+1):((r-1)*tile_scaled_size+size(tile_m,1)), ...
+                ((c-1)*tile_scaled_size+1):((c-1)*tile_scaled_size+size(tile_m,2)))=tile_m;
+            
+        end
+    end
+
+    D = bwdist(slide_mask);
+    L = watershed(D);
+    L(bwdist(slide_mask)>duct_nbh_size) = 0;
+    
+    save(fullfile(outputPath, strcat(folders{i}, '_ducts.mat')), 'slide_mask', 'L')
+    imwrite(L,fullfile(outputPath, strcat(folders{i}, '_ducts.jpg')))
+    %rgb = label2rgb(L,'jet',[.5 .5 .5]);
+    
+end
+end
