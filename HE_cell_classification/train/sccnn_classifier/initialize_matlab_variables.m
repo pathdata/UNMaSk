c29c871a9f51c943b228e517b1bd1ea6dc2b4fd0
Priya Narayanan <56313819+pathdata@users.noreply.github.com>
2021-01-04 21:03:29 +0000

    Delete initialize_matlab_variables.m

--- HE_cell_classification/train/sccnn_classifier/initialize_matlab_variables.m
+++
@@ -1,8 +0,0 @@
-clear
-%%
-addpath(genpath(fullfile(fileparts(mfilename('fullpath')), 'matlab/')));
-matlab_input = {};
-mat_file_name = '';
-results_dir = '';
-image_path = '';
-csv_detection_results_dir = '';
