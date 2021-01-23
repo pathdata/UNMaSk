3ea9a809a51a33c276594b20f09af24e6231cb0d
Priya Narayanan <56313819+pathdata@users.noreply.github.com>
2021-01-23 07:35:08 +0000

    Update script_git_reproduce.R

--- DCIS/Spatial_Analysis_DCIS/script_git_reproduce.R
+++ DCIS/Spatial_Analysis_DCIS/script_git_reproduce.R
@@ -1,26 +1,25 @@
-library(ggpubr)
-library(ggplot2)
-library(gridExtra)
+(ggpubr)
+(ggplot2)
+(gridExtra)
 
-# Figure 4
 
-t=read.csv("validData_DCIS_DAVE.csv")
-pdf(file = "Fig4v3.pdf", width = 18 , height = 8)
-p1 <- ggscatter(t,x="B.sz.IHC",y="A.sz.HE",add="reg.line",conf.int=TRUE,cor.coef = TRUE,cor.method = "spearman",xlab="Biological DCIS estimation",ylab="Automated DCIS estimation")
+t=read.csv("Dave_validData_Fig4_data.csv")
+
+p1 <- ggscatter(t,x="B.sz.IHC",y="A.sz.HE", add="reg.line",conf.int=TRUE,cor.coef = TRUE,cor.method = "spearman",xlab="Biological DCIS estimation",ylab="Automated DCIS estimation")
 #dev.off()
 
-#t=read.csv("annotation_area_v2.csv")
-#pdf(file = "Fig4d.pdf", width = 18 , height = 8)
-p2 <- ggscatter(t,x="B.Area.IHC",y="A.Area.HE",add="reg.line",conf.int=TRUE,cor.coef = TRUE,cor.method = "pearson",xlab="Biological DCIS estimation",ylab="Automated DCIS estimation")
+t=read.csv("Dave_validData_annotation_area.csv")
+pdf(file = "figures/Fig4d.pdf", width = 18 , height = 8)
+p2 <- ggscatter(t,x="IHC",y="HE", add="reg.line",conf.int=TRUE,cor.coef = TRUE, cor.method = "pearson",xlab="Biological DCIS estimation",ylab="Automated DCIS estimation")
 p3<-grid.arrange(p1,p2, nrow=1)
 dev.off()
 
 #Figure 6
 
-MS_data=read.csv('script_Fig6_data.csv',stringsAsFactors = F)
+MS_data=read.csv('Dave_validData_Fig6_data.csv',stringsAsFactors = F)
 MS_data_PD=MS_data[MS_data$Group == "Pure DCIS",]
 MS_data_AD=MS_data[MS_data$Group == "Adjacent DCIS",]
-pdf(file="MS_Lym_Pure_adjacent_Figure6.pdf",width = 12 , height = 8)
+pdf(file="figures/MS_Lym_Pure_adjacent_Figure6.pdf",width = 12 , height = 8)
 
 p1 <- ggboxplot(MS_data,x="Group",y="DCIS.MS", color="Group", palette = c("#0000FF", "#FC4E07"), line.color="gray", line.size=1.0, add="jitter")+scale_y_continuous(limits=c(0,1))+
   stat_compare_means(method="wilcox")+theme(text = element_text(size=18))
