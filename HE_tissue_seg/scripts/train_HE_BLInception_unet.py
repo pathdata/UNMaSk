c03f56a6e926fe14b1923470d22a112892116e38
Priya Narayanan <56313819+pathdata@users.noreply.github.com>
2021-04-29 20:59:48 +0100

    Update train_HE_BLInception_unet.py

--- HE_tissue_seg/scripts/train_HE_BLInception_unet.py
+++ HE_tissue_seg/scripts/train_HE_BLInception_unet.py
@@ -41,8 +41,8 @@ def plot_training_curves(output_dir, H, epochs):
     plt.style.use("seaborn-white")
     plt.figure()
     plt.subplot(211)
-    plt.plot(H.["loss"], label="train_loss")
-    plt.plot(H.["val_loss"], label="val_loss")
+    plt.plot(H.history["loss"], label="train_loss")
+    plt.plot(H.history["val_loss"], label="val_loss")
     plt.title("Training/Validation Network")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
@@ -50,8 +50,8 @@ def plot_training_curves(output_dir, H, epochs):
 
 
     plt.subplot(212)
-    plt.plot(H.["acc"], label="train_accuracy")
-    plt.plot(H.["val_acc"], label="val_accuracy")
+    plt.plot(H.history["acc"], label="train_accuracy")
+    plt.plot(H.history["val_acc"], label="val_accuracy")
     # plt.title("Training/Validation Network")
     plt.xlabel("Epochs")
     plt.ylabel("Accuracy")
